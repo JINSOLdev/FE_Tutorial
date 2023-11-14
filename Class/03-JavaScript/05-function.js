362b1e75e46f58e7a86f601a192a3821144971b9
Jinsol Kim <jinsol032360@gmail.com>
2023-11-14 11:00:19 +0900

    add. 함수(+작성법)

---
+++ Class/03-JavaScript/05-function.js
@@ -0,0 +1,12 @@
+let auth = () => {
+    const token = String(Math.floor(Math.random() * 100000)).padStart(6, "0");
+    document.getElementById("target").innerText = token;
+    document.getElementById("target").style.color = "#" + token;
+};
+
+// function auth() {
+//     const token = String(
+//         Math.floor(Math.random() * 100000)
+//     ).padStart(6, "0");
+//     document.getElementById("target").innerText = token;
+// }
