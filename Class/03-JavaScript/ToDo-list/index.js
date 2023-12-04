d76d8b57497a0f1bf2ec153a269d2d11b8c39402
Jinsol Kim <jinsol032360@gmail.com>
2023-12-04 17:13:07 +0900

    add. JS로 HTML 태그 생성

--- Class/03-JavaScript/ToDo-list/index.js
+++ Class/03-JavaScript/ToDo-list/index.js
@@ -3,6 +3,8 @@ const keyCodeCheck = function () {
     // console.log(window.event.keyCode === 13);
     if (window.event.keyCode === 13) {
         const inputValue = document.querySelector('#todo-input').value;
-        console.log(inputValue);
+        const newLi = document.createElement('li');
+        const newSpan = document.createElement('span');
+        console.log(inputValue, newLi, newSpan);
     }
 };
