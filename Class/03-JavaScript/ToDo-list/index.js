b7d360ae292611fdfe57e7b71104a35542adae5e
Jinsol Kim <jinsol032360@gmail.com>
2023-12-04 17:09:32 +0900

    add. 키보드 event 체크

--- Class/03-JavaScript/ToDo-list/index.js
+++ Class/03-JavaScript/ToDo-list/index.js
@@ -1,4 +1,8 @@
-const checkInput = function () {
-    const inputValue = document.querySelector('#todo-input').value;
-    console.log(inputValue);
+const keyCodeCheck = function () {
+    // console.log(window.event);
+    // console.log(window.event.keyCode === 13);
+    if (window.event.keyCode === 13) {
+        const inputValue = document.querySelector('#todo-input').value;
+        console.log(inputValue);
+    }
 };
