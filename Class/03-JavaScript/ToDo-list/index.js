85ad26d9fc93dd09f0babee260795db3b6d8f758
Jinsol Kim <jinsol032360@gmail.com>
2023-12-05 15:40:47 +0900

    add. addEventListener

--- Class/03-JavaScript/ToDo-list/index.js
+++ Class/03-JavaScript/ToDo-list/index.js
@@ -1,16 +1,25 @@
 const todoInput = document.querySelector('#todo-input');
 
-const keyCodeCheck = function () {
-    // console.log(window.event);
-    // console.log(window.event.keyCode === 13);
-    if (window.event.keyCode === 13 && todoInput.value !== '') {
-        const todoList = document.querySelector('#todo-list');
-        const newLi = document.createElement('li');
-        const newSpan = document.createElement('span');
+const createTodo = function () {
+    const todoList = document.querySelector('#todo-list');
+    const newLi = document.createElement('li');
+    const newSpan = document.createElement('span');
+    const newBtn = document.createElement('button');
 
-        newSpan.textContent = todoInput.value;
-        newLi.appendChild(newSpan);
-        todoList.appendChild(newLi);
-        todoInput.value = '';
+    newBtn.addEventListener('click', () => {
+        newLi.classList.toggle('complete');
+    });
+
+    newSpan.textContent = todoInput.value;
+    newLi.appendChild(newBtn);
+    newLi.appendChild(newSpan);
+    todoList.appendChild(newLi);
+    console.log(newLi);
+    todoInput.value = '';
+};
+
+const keyCodeCheck = function () {
+    if (window.event.keyCode === 13 && todoInput.value !== '') {
+        createTodo();
     }
 };
