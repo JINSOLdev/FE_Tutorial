f12d4a85ce50324ef2bc3f37fdb12f3d79198689
Jinsol Kim <jinsol032360@gmail.com>
2023-12-05 16:04:19 +0900

    add. eventListener를 활용한 태그 삭제

--- Class/03-JavaScript/ToDo-list/index.js
+++ Class/03-JavaScript/ToDo-list/index.js
@@ -10,11 +10,14 @@ const createTodo = function () {
         newLi.classList.toggle('complete');
     });
 
+    newLi.addEventListener('dblclick', () => {
+        newLi.remove();
+    });
+
     newSpan.textContent = todoInput.value;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
-    console.log(newLi);
     todoInput.value = '';
 };
 
@@ -22,4 +25,23 @@ const keyCodeCheck = function () {
     if (window.event.keyCode === 13 && todoInput.value !== '') {
         createTodo();
     }
+};
+
+const deleteAll = function () {
+    const liList = document.querySelectorAll('li');
+    for (let i = 0; i < liList.length; i++) {
+        liList[i].remove();
+    }
+};
+
+const saveItemsFn = function () {
+    const saveItems = [];
+    for (let i = 0; i < todoList.children.length; i++) {
+        const todoObj = {
+            contents: todoList.children[i].querySelector('span').textContent,
+            complete: todoList.children[i].classList.contains('complete'),
+        };
+        saveItems.push(todoObj);
+    }
+    console.log(saveItems);
 };
