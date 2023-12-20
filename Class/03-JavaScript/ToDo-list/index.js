cdda2443cb1748bd3339d1e101a588a1155b806f
Jinsol Kim <jinsol032360@gmail.com>
2023-12-20 10:58:33 +0900

    add. localStorage로 렌더링 딜레이 해결

--- Class/03-JavaScript/ToDo-list/index.js
+++ Class/03-JavaScript/ToDo-list/index.js
@@ -3,6 +3,9 @@ const todoList = document.querySelector('#todo-list');
 
 const savedTodoList = JSON.parse(localStorage.getItem('save-items'));
 
+// localstorage에 저장되어 있는 날씨 데이터 받아오는 코드
+const savedWeatherData = JSON.parse(localStorage.getItem('saved-weather'));
+
 const createTodo = function (storageData) {
     let todoContents = todoInput.value;
     if (storageData) {
@@ -75,26 +78,48 @@ if (savedTodoList) {
     }
 }
 
-const weatherSearch = function (position) {
-    console.log(position.latitude);
+const weatherDataActive = function ({ location, weather }) {
+    const weatherMainList = ['Clear', 'Clouds', 'Drizzle', 'Snow', 'Thunderstorm'];
+    weather = weatherMainList.includes(weather) ? weather : 'Fog';
+    const locationNameTag = document.querySelector('#location-name-tag');
+    locationNameTag.textContent = location;
+    // console.log(weather);
+    document.body.style.backgroundImage = `url('./images/${weather}.jpg')`;
+
+    if (!savedWeatherData || savedWeatherData.location !== location || savedWeatherData.weather !== weather) {
+        console.log('조건식 성립');
+        // shorthand property, 객체 형태인 날씨 데이터를 JSON 데이터 포맷 형태로 localStorage에 저장
+        localStorage.setItem('saved-weather', JSON.stringify({ location, weather }));
+    }
+    console.log('조건식 성립X');
+};
+
+const weatherSearch = function ({ latitude, longitude }) {
     const openWetherRes = fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&exclude={part}&appid=353e5a88844ec7475862c59decbff83d`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude={part}&appid=353e5a88844ec7475862c59decbff83d`
     )
         .then((res) => {
             return res.json();
         })
         .then((json) => {
-            console.log(json.name, json.weather[0].description);
+            console.log(json.name, json.weather[0].main);
+            const weatherData = {
+                location: json.name,
+                weather: json.weather[0].main,
+            };
+            weatherDataActive(weatherData);
         })
         .catch((err) => {
             console.log(err);
         });
 };
 
-const accessToGeo = function (position) {
+const accessToGeo = function ({ coords }) {
+    const { latitude, longitude } = coords;
+    // shorthand property
     const positionObj = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
+        latitude,
+        longitude,
     };
     weatherSearch(positionObj);
 };
@@ -107,16 +132,7 @@ const askForLocation = function () {
 
 askForLocation();
 
-// then, catch
-// const promiseTest = function () {
-//     return new Promise((resolver, reject) => {
-//         setTimeout(() => {
-//             resolver('success');
-//             // reject('error');
-//         }, 2000);
-//     });
-// };
-
-// promiseTest().then((res) => {
-//     console.log(res);
-// });
+// localStorage에 저장된 날씨 데이터가 있는지 여부에 따라 최초 렌더링시에 weatherDataActive()함수를 localStorage에 저장된 데이터를 기반으로 실행하는 코드
+if (savedWeatherData) {
+    weatherDataActive(savedWeatherData);
+}
