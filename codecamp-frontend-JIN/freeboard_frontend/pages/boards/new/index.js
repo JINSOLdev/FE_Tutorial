9adfe56d7ec2763f4e7f23bf83e3ecde33a62a71
Jinsol Kim <jinsol032360@gmail.com>
2023-12-27 13:39:34 +0900

    Error 태그 추가, 컴포넌트 수정

--- codecamp-frontend-JIN/freeboard_frontend/pages/boards/new/index.js
+++ codecamp-frontend-JIN/freeboard_frontend/pages/boards/new/index.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import {
     Address,
     ButtonWrapper,
@@ -21,54 +20,70 @@ import {
     Youtube,
     Zipcode,
     ZipcodeWrapper,
+    Error,
 } from '../../../styles/boardsNew';
+import { useState } from 'react';
 
 // 컴포넌트!
 export default function BoardsNewPage() {
-    const [writer, setWriter] = useState();
-    const [password, setPassword] = useState();
-    const [subject, setSubject] = useState();
-    const [content, setContents] = useState();
-    const [zipcode, setZipcode] = useState();
-    const [address, setAddress] = useState();
-    const [youtube, setYoutube] = useState();
+    const [writer, setWriter] = useState('');
+    const [password, setPassword] = useState('');
+    const [title, setTitle] = useState('');
+    const [contents, setContents] = useState('');
 
-    const [writerError, setWriterError] = useState();
+    const [writerError, setWriterError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [titleError, setTitleError] = useState('');
+    const [contentsError, setContentsError] = useState('');
 
     function onChangeWriter(event) {
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.value);
+        // console.log(event);
+        // console.log(event.target);
+        // console.log(event.target.value);
 
         setWriter(event.target.value);
+        if (event.target.value !== '') {
+            setWriterError('');
+        }
     }
 
     function onChangePassword(event) {
         setPassword(event.target.value);
+        if (event.target.value !== '') {
+            setPasswordError('');
+        }
     }
 
-    function onChangeSubject(event) {
-        setSubject(event.target.value);
+    function onChangeTitle(event) {
+        setTitle(event.target.value);
+        if (event.target.value !== '') {
+            setTitleError;
+        }
     }
 
-    function onChangeContent(event) {
+    function onChangeContents(event) {
         setContents(event.target.value);
-    }
-
-    function onChangeZipcode(event) {
-        setZipcode(event.target.value);
-    }
-
-    function onChangeAddress(event) {
-        setAddress(event.target.value);
-    }
-
-    function onChangeYoutube(event) {
-        setYoutube(event.target.value);
+        if (event.target.value !== '') {
+            setContentsError('');
+        }
     }
 
     function onClickSubmit(event) {
-        setSubmit(event.target.value);
+        if (!writer) {
+            setWriterError('작성자를 입력해주세요.');
+        }
+        if (!password) {
+            setPasswordError('비밀번호를 입력해주세요.');
+        }
+        if (!title) {
+            setTitleError('제목을 입력해주세요.');
+        }
+        if (!contents) {
+            setContentsError('내용을 입력해주세요.');
+        }
+        if (writer && password && title && contents) {
+            alert('게시글이 등록되었습니다.');
+        }
     }
 
     return (
@@ -79,33 +94,36 @@ export default function BoardsNewPage() {
                 <InputWrapper>
                     <Label>작성자</Label>
                     <Writer type="text" placeholder="이름을 적어주세요." onChange={onChangeWriter} />
-                    <div>{setWriterError}</div>
+                    <Error>{writerError}</Error>
                 </InputWrapper>
                 <InputWrapper>
                     <Label>비밀번호</Label>
                     <Password type="password" placeholder="비밀번호를 작성해주세요." onChange={onChangePassword} />
+                    <Error>{passwordError}</Error>
                 </InputWrapper>
             </WriterWrapper>
             <InputWrapper>
                 <Label>제목</Label>
-                <Subject type="text" placeholder="제목을 작성해주세요." onChange={onChangeSubject} />
+                <Subject type="text" placeholder="제목을 작성해주세요." onChange={onChangeTitle} />
+                <Error>{titleError}</Error>
             </InputWrapper>
             <InputWrapper>
                 <Label>내용</Label>
-                <Contents placeholder="내용을 작성해주세요." onChange={onChangeContent} />
+                <Contents placeholder="내용을 작성해주세요." onChange={onChangeContents} />
+                <Error>{contentsError}</Error>
             </InputWrapper>
             <InputWrapper>
                 <Label>주소</Label>
                 <ZipcodeWrapper>
-                    <Zipcode placeholder="07250" onChange={onChangeZipcode} />
+                    <Zipcode placeholder="07250" />
                     <SearchButton>우편번호 검색</SearchButton>
                 </ZipcodeWrapper>
-                <Address onChange={onChangeAddress} />
-                <Address onChange={onChangeAddress} />
+                <Address />
+                <Address />
             </InputWrapper>
             <InputWrapper>
                 <Label>유튜브</Label>
-                <Youtube placeholder="링크를 복사해주세요." onChange={onChangeYoutube} />
+                <Youtube placeholder="링크를 복사해주세요." />
             </InputWrapper>
             <ImageWrapper>
                 <Label>사진첨부</Label>
