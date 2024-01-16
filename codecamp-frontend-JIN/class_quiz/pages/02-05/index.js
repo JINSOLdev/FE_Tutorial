79fcfd9668aaff6adb4e20022a4119d29bb8e9d4
Jinsol Kim <jinsol032360@gmail.com>
2024-01-16 11:07:02 +0900

    [Quiz]section05_잇츠로드

--- codecamp-frontend-JIN/class_quiz/pages/02-05/index.js
+++ codecamp-frontend-JIN/class_quiz/pages/02-05/index.js
@@ -2,44 +2,91 @@ import { useState } from 'react';
 import {
     Container,
     Wrapper,
-    TitleImg,
-    Title,
-    WriterWrapper,
-    Email,
-    Password,
-    LoginWrapper,
+    LogoWrapper,
+    LogoImg,
+    LogoTitle,
+    InputWrapper,
+    EmailInputWrapper,
+    EmailInput,
+    EmailError,
+    PasswordInputWrapper,
+    PasswordInput,
+    PasswordError,
     LoginButton,
-    OptionalWrapper,
-    EmailButton,
-    PasswordButton,
-    RegisterButton,
-    SocialLoginWrapper,
-    KakaoTalkButton,
+    LoginButtonTitle,
+    EtcButton,
+    EtcItem,
+    SocialLoginButton,
+    SocialLoginButtonImg,
+    SocialLoginButtonTitle,
 } from '../../styles/ItsRoadPage.js';
 
 export default function ItsRoadPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+    // 이메일 검증
+    function onChangeEmail(event) {
+        setEmail(event.target.value);
+        if (!email.includes('@')) {
+            setEmailError('이메일 주소를 다시 확인해주세요❗');
+        } else {
+            setEmailError('');
+        }
+    }
+
+    // 비밀번호 검증
+    function onChangePassword(event) {
+        setPassword(event.target.value);
+        if (password.length >= 8 && password.length <= 16) {
+            setPasswordError('');
+        } else {
+            setPasswordError('8~16자의 영문, 숫자, 특수 문자만 사용 가능합니다❗');
+        }
+    }
+
+    // 로그인
+    function onClickLogin() {
+        if (emailError === '' && passwordError === '') {
+            alert('환영합니다🎉');
+        }
+    }
+
     return (
         <Container>
             <Wrapper>
-                <TitleImg src='img/pizza.png'></TitleImg>
-                <Title>잇츠로드</Title>
-                <WriterWrapper>
-                    <Email type="text" placeholder="이메일을 입력해주세요." />
-                    {/* <div>이메일 주소를 다시 확인해주세요.</div> */}
-                    <Password type="password" placeholder="비밀번호를 입력해주세요" />
-                    {/* <div>8~16자의 영문, 숫자, 특수 문자만 사용 가능합니다.</div> */}
-                </WriterWrapper>
-                <LoginWrapper>
-                    <LoginButton>로그인</LoginButton>
-                </LoginWrapper>
-                <OptionalWrapper>
-                    <EmailButton>이메일 찾기</EmailButton>
-                    <PasswordButton>비밀번호 찾기</PasswordButton>
-                    <RegisterButton>회원가입</RegisterButton>
-                </OptionalWrapper>
-                <SocialLoginWrapper>
-                    <KakaoTalkButton>카카오톡으로 로그인</KakaoTalkButton>
-                </SocialLoginWrapper>
+                <LogoWrapper>
+                    <LogoImg src="/img/favicon.ico"></LogoImg>
+                    <LogoTitle>잇츠로드</LogoTitle>
+                </LogoWrapper>
+                <InputWrapper>
+                    <EmailInputWrapper>
+                        <EmailInput type="text" placeholder="이메일을 입력해주세요." onChange={onChangeEmail} />
+                        {/* <div>이메일 주소를 다시 확인해주세요.</div> */}
+                        <EmailError>{emailError}</EmailError>
+                    </EmailInputWrapper>
+                    <PasswordInputWrapper>
+                        <PasswordInput type="password" placeholder="비밀번호를 입력해주세요" onChange={onChangePassword} />
+                        {/* <div>8~16자의 영문, 숫자, 특수 문자만 사용 가능합니다.</div> */}
+                        <PasswordError>{passwordError}</PasswordError>
+                    </PasswordInputWrapper>
+                </InputWrapper>
+                <LoginButton onClick={onClickLogin}>
+                    <LoginButtonTitle>로그인</LoginButtonTitle>
+                </LoginButton>
+                <EtcButton>
+                    <EtcItem>이메일 찾기</EtcItem>
+                    <EtcItem>|</EtcItem>
+                    <EtcItem>비밀번호 찾기</EtcItem>
+                    <EtcItem>|</EtcItem>
+                    <EtcItem>회원가입</EtcItem>
+                </EtcButton>
+                <SocialLoginButton>
+                    <SocialLoginButtonImg src="/public/kakao.png"></SocialLoginButtonImg>
+                    <SocialLoginButtonTitle>카카오톡으로 로그인</SocialLoginButtonTitle>
+                </SocialLoginButton>
             </Wrapper>
         </Container>
     );
--- codecamp-frontend-JIN/class_quiz/styles/ItsRoadPage.js
+++ codecamp-frontend-JIN/class_quiz/styles/ItsRoadPage.js
@@ -1,101 +1,133 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
 
 export const Container = styled.div`
     display: flex;
     justify-content: center;
     padding: 100px;
 `;
+
 export const Wrapper = styled.div`
     width: 640px;
     padding: 0 50px 0 50px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    background-image: url('/img/pizza.png')
-    background-color: rgba(0, 0, 0, 0.55);
-    border: 1px solid gray;
+    background-image: url('/public/pizza.png');
+    background-color: ivory;
+    background-blend-mode: multiply;
 `;
 
-export const TitleImg = styled.div`
+export const LogoWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 220px;
+    padding-bottom: 160px;
+`;
+
+export const LogoImg = styled.img`
     width: 100px;
     height: 100px;
 `;
 
-export const Title = styled.div`
+export const LogoTitle = styled.div`
     font-size: 60px;
     font-weight: bold;
-    color: black;
+    color: gray;
     padding-top: 30px;
 `;
 
-export const WriterWrapper = styled.div`
+export const InputWrapper = styled.div`
+    width: 540px;
+`;
+export const EmailInputWrapper = styled.div`
+    border-bottom: 1px solid gray;
+    padding-bottom: 20px;
+    margin-bottom: 10px;
+`;
+export const EmailInput = styled.input`
+    width: 80%;
+    font-size: 24px;
+    color: gray;
+    border: none;
+    background: none;
+    outline: none;
+`;
+export const PasswordInputWrapper = styled.div`
+    border-bottom: 1px solid gray;
+    padding-bottom: 20px;
+    margin-bottom: 10px;
+`;
+export const PasswordInput = styled.input`
+    width: 80%;
+    margin-top: 40px;
+    font-size: 24px;
+    color: gray;
+    border: none;
+    background: none;
+    outline: none;
+`;
+export const EmailError = styled.div`
+    padding-top: 10px;
+    font-size: 18pxl
+    color: #ff1b6d;
+`;
+export const PasswordError = styled.div`
+    padding-top: 10px;
+    font-size: 18pxl
+    color: #ff1b6d; 
+`;
+export const LoginButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 76px;
+    border-radius: 38px;
+    padding: 25px 0;
+    background-color: rgba(255, 27, 109, 0.6);
+    margin: 20px 0 45px 0;
+    cursor: pointer;
+`;
+export const LoginButtonTitle = styled.div`
+    opacity: 1;
+    font-size: 26px;
+    font-weight: bold;
+    color: #fff;
+`;
+export const EtcButton = styled.button`
     width: 100%;
     display: flex;
-    flex-direction: column;
+    justify-content: space-evenly;
+    border: none;
+    background: none;
+`;
+export const EtcItem = styled.div`
+    font-size: 20px;
+    color: gray;
+    cursor: pointer;
+`;
+export const SocialLoginButton = styled.button`
+    display: flex;
     align-items: center;
-    margin-top: 250px;
-`;
-
-export const Email = styled.input`
-    width: 540px;
-    margin-top: 365px;
-    border: 1px solid #7d7d7d;
-`;
-
-export const Password = styled.input`
-    width: 540px;
-    margin-top: 50px;
-    left: -255px;
-    border: 1px solid #7d7d7d;
-`;
-
-export const LoginWrapper = styled.div`
-    margin-top: 50px;
-`;
-
-export const LoginButton = styled.button`
-    width: 540px;
+    justify-content: center;
+    width: 100%;
     height: 76px;
     border-radius: 38px;
-    border: 1px solid #ff1b6d;
+    padding: 25px 0;
+    background: none;
+    color: #fae100;
+    border: 2px solid #fea100;
+    margin: 60px 0 80px 0;
+    cursor: pointer;
 `;
-
-export const OptionalWrapper = styled.div`
-    margin-top: 50px;
-    justify-content: space-between;
+export const SocialLoginButtonImg = styled.img`
+    width: 32px;
+    height: 30px;
+    margin-right: 20px;
 `;
-
-export const EmailButton = styled.button`
-    font: Noto sans KR;
-    weight: 700;
-    size: 20px;
-    line height: 28.96px;
-    color: #ffffff;
-`;
-
-export const PasswordButton = styled.button`
-    font: Noto sans KR;
-    weight: 700;
-    size: 20px;
-    line height: 28.96px;
-    color: #ffffff;`;
-
-export const RegisterButton = styled.button`
-    font: Noto sans KR;
-    weight: 700;
-    size: 20px;
-    line height: 28.96px;
-    color: #ffffff;`;
-
-export const SocialLoginWrapper = styled.div`
-    margin-top: 30px;
-`;
-
-export const KakaoTalkButton = styled.button`
-    width: 540px;
-    height: 76px;
-    border: 2px solid #fae100;
-    border-radius: 38px;
-    opacity: 60%;
+export const SocialLoginButtonTitle = styled.div`
+    opacity: 1;
+    font-size: 26px;
+    font-weight: bold;
 `;
